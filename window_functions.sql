71cdf23dd2a1f2313d9eb3acd0b0c63550fc7003
Barry Griffith <barrygriffith@outlook.com>
2025-08-04 15:50:08 -0400

    Assignment complete

---
+++ window_functions.sql
@@ -0,0 +1,35 @@
+-- Use a window function to add columns showing:
+-- The maximum population (max_pop) for each county.
+-- The minimum population (min_pop) for each county.
+
+SELECT *,
+		MAX(population) OVER(PARTITION BY county) AS max_pop,
+		MIN(population) OVER(PARTITION BY county) AS min_pop
+FROM population;
+
+
+-- Rank counties from largest to smallest population for each year.
+
+SELECT *,
+		RANK() OVER(PARTITION BY year ORDER BY population DESC) AS pop_rank
+FROM population;
+
+
+-- Use the unemployment table:
+-- Calculate the rolling 12-month average unemployment rate using the unemployment table.
+-- Include the current month and the preceding 11 months.
+-- Hint: Reference two columns in the ORDER BY argument (year and period).
+
+SELECT *,
+	ROUND(AVG(value) OVER(PARTITION BY county ORDER BY year, period ROWS BETWEEN 11 PRECEDING AND CURRENT ROW), 2) AS month_avg
+FROM unemployment;
+
+
+
+
+
+
+
+
+
+
